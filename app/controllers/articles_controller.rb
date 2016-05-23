0c44e42d3a25f4d1295d5cf87b827e4eeb807705
Rashit Akhtyamov <rashit157@gmail.com>
2016-05-23 22:49:33 +0300

    error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,31 @@
 class ArticlesController < ApplicationController
-	def new
-		@article = Article.new
-	end
-
-	def edit
-		@article = Article.find(params[:id])
-
-	def create
-	  @article = Article.new(article_params)
-	  
-	  if @article.save
-	  redirect_to @article
-	  else
-	  	render 'new'
-	  end	
-	end
   
-  def show
-		@article = Article.find(params[:id])
-	end	
-
-	def index 
-		@articles = Article.all
+  def index 
+	@articles = Article.all
   end
 
+  def show
+	@article = Article.find(params[:id])
+  end  	
+
+
+  def new
+	@article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end  
+
+  def create
+	@article = Article.new(article_params)
+	  
+	if @article.save
+	redirect_to @article
+	else
+	  render 'new'
+  end	
+    
   def update
   	@article = Article.find(params[:id])
 
@@ -31,11 +33,11 @@ class ArticlesController < ApplicationController
   		redirect_to @article
   	else
   	  render 'edit'
-  	end  	
+  	end
+  end	  	
 
-		private
+    private
 
-	  def article_params
-	  	params.require(:article).permit(:title, :text)
-		end
-end
+      def article_params
+	    params.require(:article).permit(:title, :text)
+      end
