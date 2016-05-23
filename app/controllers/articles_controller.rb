e59417a08002375312a49404842941c7eb643652
Rashit Akhtyamov <rashit157@gmail.com>
2016-05-23 16:52:59 +0300

    added view for action index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,16 @@ class ArticlesController < ApplicationController
 	  @article.save
 	  redirect_to @article
 	end
-
-	def show
+  
+  def show
 		@article = Article.find(params[:id])
 	end	
 
-	private
+	def index 
+		@articles = Article.all
+  end
+
+		private
 
 	  def article_params
 	  	params.require(:article).permit(:title, :text)
