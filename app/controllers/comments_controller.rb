8fd03313dc4e637811fe5fe1c778f61a66d89217
Rashit Akhtyamov <rashit157@gmail.com>
2016-05-24 14:47:53 +0300

    delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,12 @@ class CommentsController < ApplicationController
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
- 
+
+  def destroy
+  	@article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+    
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
