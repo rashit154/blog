771034660ace7f2f31ec850672d313eafbcdce26
Rashit Akhtyamov <rashit157@gmail.com>
2016-05-24 13:47:51 +0300

    you can add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+	def create
+		@article = Article.find(params[:article_id])
+		@comment = @article.comment.create(comment_params)
+		redirect_to article_path(@aritcle)
+	end
+
+	private
+	  def comments_params
+	  	params.require(:comment).permit(:comment, :body)
+	  end
 end
+
+
